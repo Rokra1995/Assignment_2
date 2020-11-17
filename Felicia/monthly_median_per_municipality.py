e57b4912b313fd2b8b24ac2f1312240d54df9be0
feliciabetten <feliciabetten.fb@gmail.com>
2020-11-17 14:23:45 +0100

    sql query 2 and 5 and tourist_info

--- Felicia/monthly_median_per_municipality.py
+++ Felicia/monthly_median_per_municipality.py
@@ -1,34 +1,35 @@
+# SQL query 5 - Felicia
+# For  every  municipality and  every  month  in  2018:  
+# the  absolute  difference  between  the  median  house  price  for  that month in that municipality and the median house price for the next month in that municipality
+# tables: funda sellingPrice, zipcodes and municipality_names 
+# variables: median of sellingPrice per municipalityname and monthly division 
+
 import pandas.io.sql as sqlio
 import pandas as pd
 import psycopg2
 import mysql.connector
 
-def funda_analysis():
-    #start connection with database
+def monthly_median_per_municipality_calculation():
+    #Start connection with database
     with open ('db_login.txt', 'r') as myfile:
         data = myfile.read()
     conn = psycopg2.connect(data)
     cur = conn.cursor()
 
-    # Felicia: For  every  municipality and  every  month  in  2018:  
-    # the  absolute  difference  between  the  median  house  price  for  that month in that municipality and the median house price for the next month in that municipality
-    # tables: funda sellingPrice, zipcodes and municipality_names 
-    # needed values: median of sellingPrice per municipalityname and monthly division 
-
     monthly_median_per_municipality_table = "SELECT sellingPrice, publicationDate, MunicipalityName FROM funda NATURAL LEFT JOIN zipcodes NATURAL LEFT JOIN municipality_names"
     monthly_median_per_municipality = sqlio.read_sql_query(monthly_median_per_municipality_table, conn)
     
-    # group by municipality and month
-    # set publicationdate to datetime_format
+    #Group by municipality and month
+    #Set publicationdate to datetime_format
     monthly_median_per_municipality['publicationdate'] = pd.to_datetime(monthly_median_per_municipality['publicationdate'])
     
-    # calculate difference between the prices
-    # monthly median per municipality
-    # print(monthly_median_per_municipality.set_index("publicationdate").groupby(['municipalityname', pd.Grouper(freq='M')]).median()) 
-    # difference between #monthly median per municipality
-    # print(monthly_median_per_municipality.set_index("publicationdate").groupby(['municipalityname', pd.Grouper(freq='M')]).median().diff()) 
+    #Calculate difference between the prices
+    #Monthly median per municipality
+    #print(monthly_median_per_municipality.set_index("publicationdate").groupby(['municipalityname', pd.Grouper(freq='M')]).median()) 
+    #Difference between monthly median per municipality
+    #print(monthly_median_per_municipality.set_index("publicationdate").groupby(['municipalityname', pd.Grouper(freq='M')]).median().diff()) 
     
-    # calculate absolute difference between the prices
+    #Calculate absolute difference between the prices
     print(monthly_median_per_municipality.set_index("publicationdate").groupby(['municipalityname', pd.Grouper(freq='M')]).median().diff().abs())
     
     #Make changes to db persistent
@@ -37,6 +38,6 @@ def funda_analysis():
     #End connection
     cur.close()
     conn.close()
-    return print('Funda Data succesfully added')
-funda_analysis()
+    return print('Monthly median sellingprice per municipality succesfully calculated')
+monthly_median_per_municipality_calculation()
 
--- Felicia/text_function.py
+++ Felicia/text_function.py
@@ -1,3 +1,5 @@
 # Create the text search function to the python wrapper
 
 # What is the mean idea?
+
+# look up in the booklet -> pass a word in the function and look in which description this is 
