e77f4e03c5153c5a27e38bacc60500d3b978a143
feliciabetten <feliciabetten.fb@gmail.com>
2020-11-18 17:47:34 +0100

    final push!

--- Python_wrapper/wrapper.py
+++ Python_wrapper/wrapper.py
@@ -627,7 +627,7 @@ def add_tourist_info_to_database():
     conn.close()
     return print('Done')
 
-def tourist_info_analysis ():
+def tourist_info_analysis():
     #Start connection with database
     with open ('db_login.txt', 'r') as myfile:
         data = myfile.read()
@@ -647,6 +647,40 @@ def tourist_info_analysis ():
     1) correlation number_of_national_monuments+sellingprice = 0.271252 (slightly positivie) 
     2) correlation number_of_national_monuments+sellingtime = -0.155473 (slightly negative) 
     '''
+    
+    #Make changes to db persistent
+    conn.commit()
+
+    #End connection
+    cur.close()
+    conn.close()
+    return print('Done')
+
+def text_search():
+    #Start connection with database
+    with open ('db_login.txt', 'r') as myfile:
+        data = myfile.read()
+    conn = psycopg2.connect(data)
+    cur = conn.cursor()
+    
+    #Create dataframe to select columns of housing_data
+    word_in_description_table = "SELECT sellingPrice, fullDescription, houseType, categoryObject, yearOfBuilding, garden, parcelSurface, numberRooms, numberBathrooms, energylabelClass, surface FROM funda LIMIT 100;"
+    word_in_description = sqlio.read_sql_query(word_in_description_table, conn)
+    
+    input_limit = 3
+    counter = 0
+    while counter < input_limit:
+        word = input("Describe in one word what your house should look like: ")
+        if not re.match("^[a-zA-Z\s]*$", word):
+            print("Error! Only letters a-z allowed!")
+            counter += 1
+        elif " " in word:
+            print("Error! Only 1 word allowed!")
+            counter += 1
+        else:
+            print(word_in_description[word_in_description.fulldescription.str.contains(word)])
+            counter = input_limit
+    
     
     #Make changes to db persistent
     conn.commit()
